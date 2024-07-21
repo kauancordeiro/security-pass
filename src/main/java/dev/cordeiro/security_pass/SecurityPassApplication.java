9c96ba4c0cdc6b504cd1a00a7a98bd9b9e008b78
Kauan <cvitorcordeiro@gmail.com>
2024-07-21 19:24:22 -0300

    First commit

---
+++ src/main/java/dev/cordeiro/security_pass/SecurityPassApplication.java
@@ -0,0 +1,13 @@
+package dev.cordeiro.security_pass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityPassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityPassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/cordeiro/security_pass/SecurityPassApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cordeiro.security_pass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityPassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
